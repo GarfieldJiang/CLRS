2513949f39581475bdd8c1f7177acdb1318fdfb2
jehovah0121 <jehovah0121@gmail.com>
2017-07-02 17:42:49 +0800

    DP: problem 15-3
    A better implementation.

--- DP/problem_15_3.py
+++ DP/problem_15_3.py
@@ -1,6 +1,10 @@
 # This Python file uses the following encoding: utf-8
 import math
 import unittest
+import logging
+
+
+logging.basicConfig(level=logging.WARN)
 
 
 def get_shortest_bitonic_path(points):
@@ -65,6 +69,71 @@ def get_shortest_bitonic_path(points):
 
     path_indices = [0] + l2r_indices + [n - 1] + r2l_indices + [0]
     return lengths[len(lengths) - 1], tuple([points[i] for i in path_indices])
+
+
+def get_shortest_bitonic_path_2(points):
+    if not points:
+        return 0, ()
+
+    if len(points) == 1:
+        return 0, (points[0],)
+
+    logging.debug(points)
+    n = len(points)
+    distances = [[0 for _ in xrange(0, n)] for _ in xrange(0, n)]
+    for i in xrange(0, n - 1):
+        for j in xrange(i + 1, n):
+            distances[i][j] = distances[j][i] = __get_euclidean_distance(points[i], points[j])
+
+    path_lengths = [[float('inf') for _ in xrange(0, n)] for _ in xrange(0, n)]
+    predecessors = [[-1 for _ in xrange(0, n)] for _ in xrange(0, n)]
+    for i in xrange(0, n - 1):
+        for j in xrange(i + 1, n):
+            if i == 0 and j == 1:
+                path_lengths[i][j] = distances[i][j]
+                predecessors[i][j] = 0
+                logging.debug('i=%d, j=%d, path_length=%.2f, predecessor=%d' % (
+                    i, j, path_lengths[i][j], predecessors[i][j]
+                ))
+                continue
+
+            if j == i + 1:
+                for k in xrange(0, i):
+                    new_l = path_lengths[k][i] + distances[k][i + 1]
+                    if new_l < path_lengths[i][j]:
+                        path_lengths[i][j] = new_l
+                        predecessors[i][j] = k
+                logging.debug('i=%d, j=%d, path_length=%.2f, predecessor=%d' % (
+                    i, j, path_lengths[i][j], predecessors[i][j]
+                ))
+                continue
+
+            path_lengths[i][j] = path_lengths[i][j - 1] + distances[j - 1][j]
+            predecessors[i][j] = j - 1
+            logging.debug('i=%d, j=%d, path_length=%.2f, predecessor=%d' % (
+                i, j, path_lengths[i][j], predecessors[i][j]
+            ))
+
+    tour_length = path_lengths[n - 2][n - 1] + distances[n - 2][n - 1]
+    j = n - 1
+    i = n - 2
+    tour_a = [i]
+    tour_b = [j]
+    while predecessors[i][j] >= 0:
+        k = predecessors[i][j]
+        tour_b.append(k)
+        if k < i:
+            i, j = k, i
+            tour_a, tour_b = tour_b, tour_a
+        else:
+            i, j = i, k
+
+    path = []
+    for i in xrange(len(tour_a) - 1, -1, -1):
+        path.append(points[tour_a[i]])
+    for i in xrange(0, len(tour_b)):
+        path.append(points[tour_b[i]])
+    return tour_length, tuple(path)
 
 
 def get_shortest_bitonic_path_naive(points):
@@ -140,22 +209,24 @@ def __get_euclidean_distance(a, b):
 class TestProblems(unittest.TestCase):
     def test_get_shortest_bitonic_path(self):
         cases = (
-            (),
-            ((0, 0),),
-            ((0, 0), (1, 1)),
-            ((0, 0), (1, -1), (2, 1), (3, 0)),
-            ((0, 0), (1, -6), (2, -3), (5, -2), (6, -5), (7, -1), (8, -4)),
-            ((0, 0), (1, 1), (2, -1), (3, 1), (4, -1), (5, 0)),
-            ((0, 0), (1, 2), (2, 1), (3, 5), (4, 0)),
-            ((0, 0), (1, 1), (2, 1), (3, 1), (4, 1), (5, 5), (6, 0)),
-            ((0, 0), (1, 5), (2, 1), (3, 1), (4, 1), (5, 1), (6, 0)),
+            ('Empty', ()),
+            ('Single', ((0, 0),)),
+            ('Double', ((0, 0), (1, 1))),
+            ('4 points', ((0, 0), (1, -1), (2, 1), (3, 0))),
+            ('7 points #0', ((0, 0), (1, -6), (2, -3), (5, -2), (6, -5), (7, -1), (8, -4))),
+            ('6 points', ((0, 0), (1, 1), (2, -1), (3, 1), (4, -1), (5, 0))),
+            ('5 points', ((0, 0), (1, 2), (2, 1), (3, 5), (4, 0))),
+            ('7 points #1', ((0, 0), (1, 1), (2, 1), (3, 1), (4, 1), (5, 5), (6, 0))),
+            ('7 points #2', ((0, 0), (1, 5), (2, 1), (3, 1), (4, 1), (5, 1), (6, 0))),
         )
 
-        for case in cases:
-            print "points: ", case
-            length, path = get_shortest_bitonic_path(case)
-            print "path: ", path
+        for desc, case in cases:
+            length, path = get_shortest_bitonic_path_2(case)
             length_ref, path_ref = get_shortest_bitonic_path_naive(case)
-            # print "path ref: ", path_ref
-            self.assertAlmostEqual(length, length_ref)
-            self.assertEqual(path, path_ref)
+            self.assertAlmostEqual(length, length_ref,
+                                   msg='%s, length: %.2f != %.2f' % (desc, length, length_ref))
+            reversed_path_ref = list(path_ref)
+            reversed_path_ref.reverse()
+            reversed_path_ref = tuple(reversed_path_ref)
+            self.assertTrue(path == path_ref or path == reversed_path_ref,
+                            msg='%s, path: %s != %s' % (desc, path, path_ref))
