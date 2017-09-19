7d6115fa4521619751550eb13f8f44019657934d
jehovah0121 <jehovah0121@gmail.com>
2017-09-19 21:47:00 +0800

    Greedy: binary huffman code

---
+++ Common/tree.py
@@ -0,0 +1,136 @@
+from unittest import TestCase
+
+
+class GenericTreeNode(object):
+    def __init__(self, child_capacity):
+        assert isinstance(child_capacity, int) and child_capacity > 0
+        self._children = [None for _ in xrange(0, child_capacity)]
+        self._parent = None
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, value):
+        assert value is None or type(value) == type(self)
+        self._parent = value
+
+    @property
+    def child_capacity(self):
+        return len(self._children)
+
+    def __getitem__(self, key):
+        return self._children[key]
+
+    def __setitem__(self, key, value):
+        self._children[key] = value
+
+
+class BinaryTreeNode(GenericTreeNode):
+    def __init__(self):
+        super(BinaryTreeNode, self).__init__(2)
+
+    @property
+    def left(self):
+        return self[0]
+
+    @left.setter
+    def left(self, value):
+        assert value is None or type(value) == type(self)
+        self[0] = value
+
+    @property
+    def right(self):
+        return self[1]
+
+    @right.setter
+    def right(self, value):
+        assert value is None or type(value) == type(self)
+        self[1] = value
+
+
+class TernaryTreeNode(GenericTreeNode):
+    def __init__(self):
+        super(TernaryTreeNode, self).__init__(3)
+
+    @property
+    def left(self):
+        return self[0]
+
+    @left.setter
+    def left(self, value):
+        assert value is None or type(value) == type(self)
+        self[0] = value
+
+    @property
+    def middle(self):
+        return self[1]
+
+    @middle.setter
+    def middle(self, value):
+        assert value is None or type(value) == type(self)
+        self[1] = value
+
+    @property
+    def right(self):
+        return self[2]
+
+    @right.setter
+    def right(self, value):
+        assert value is None or type(value) == type(self)
+        self[2] = value
+
+
+def pre_order_traverse_tree(root, visit):
+    if not root:
+        return
+
+    visit(root)
+    for i in xrange(0, root.child_capacity):
+        pre_order_traverse_tree(root[i], visit)
+
+
+class TestTree(TestCase):
+    class TernaryTreeNodeWithValue(TernaryTreeNode):
+        def __init__(self, value):
+            super(TestTree.TernaryTreeNodeWithValue, self).__init__()
+            self._value = value
+
+        @property
+        def value(self):
+            return self._value
+
+    def test_pre_order_traverse_ternary_tree(self):
+        root = self.TernaryTreeNodeWithValue(1)
+
+        left = self.TernaryTreeNodeWithValue(2)
+        root.left = left
+        left.parent = root
+        node_3 = self.TernaryTreeNodeWithValue(3)
+        node_4 = self.TernaryTreeNodeWithValue(4)
+        node_5 = self.TernaryTreeNodeWithValue(5)
+        left.left = node_3
+        node_3.parent = left
+        left.middle = node_4
+        node_4.parent = left
+        left.right = node_5
+        node_5.parent = left
+
+        right = self.TernaryTreeNodeWithValue(6)
+        root.right = right
+        right.parent = root
+        node_7 = self.TernaryTreeNodeWithValue(7)
+        node_8 = self.TernaryTreeNodeWithValue(8)
+        right.left = node_7
+        node_7.parent = right
+        right.middle = node_8
+        node_8.parent = right
+
+        result = []
+
+        def visit(node):
+            result.append(node.value)
+
+        pre_order_traverse_tree(root, visit)
+        self.assertEqual(result, [1, 2, 3, 4, 5, 6, 7, 8])
---
+++ Greedy/huffman_code.py
@@ -0,0 +1,107 @@
+from Common.tree import BinaryTreeNode, pre_order_traverse_tree
+from collections import namedtuple
+from unittest import TestCase
+from HeapSort.priority_queue import MaxPriorityQueue
+import logging
+
+
+logging.getLogger().setLevel(logging.INFO)
+
+
+class BinaryCodeTreeNode(BinaryTreeNode):
+    def __init__(self, symbol, freq):
+        super(BinaryCodeTreeNode, self).__init__()
+        self._symbol = symbol
+        assert freq >= 0
+        self._freq = freq
+
+    @property
+    def symbol(self):
+        return self._symbol
+
+    @property
+    def freq(self):
+        return self._freq
+
+
+def build_binary_huffman_tree(symbol_freqs):
+    if not symbol_freqs:
+        return None
+
+    n = len(symbol_freqs)
+    nodes = [None for _ in xrange(0, n)]
+    for i in xrange(0, n):
+        nodes[i] = BinaryCodeTreeNode(i, symbol_freqs[i])
+    pq = MaxPriorityQueue(nodes, key=lambda node: -node.freq)
+    while len(pq) > 1:
+        a = pq.extract_max()
+        logging.debug('symbol=%s, freq=%.2f' % ('<None>' if a.symbol is None else a.symbol, a.freq))
+        b = pq.extract_max()
+        logging.debug('symbol=%s, freq=%.2f' % ('<None>' if b.symbol is None else b.symbol, b.freq))
+        new_node = BinaryCodeTreeNode(None, a.freq + b.freq)
+        new_node.left = a
+        new_node.right = b
+        a.parent = b.parent = new_node
+        pq.insert(new_node)
+
+    return pq.extract_max()
+
+
+def get_binary_huffman_code_len(root):
+    my_dict = {'ret': 0}
+
+    def visit(node):
+        if node.left or node.right:
+            return
+
+        current = node
+        code_len = -1
+        while current:
+            code_len += 1
+            current = current.parent
+
+        logging.debug('symbol=%s, freq=%.2f, code_len=%d' % (node.symbol, node.freq, code_len))
+        my_dict['ret'] += node.freq * code_len
+
+    pre_order_traverse_tree(root, visit)
+    return my_dict['ret']
+
+
+class TestHuffmanCode(TestCase):
+    def _check_binary_code_tree(self, root, symbol_freqs, desc):
+        self.assertTrue(root is None or isinstance(root, BinaryCodeTreeNode),
+                        msg='%s, invalid root' % desc)
+        if not symbol_freqs:
+            self.assertIsNone(root, msg='%s, root should be none' % desc)
+
+        symbol_set = set(range(0, len(symbol_freqs)))
+
+        def visit(node):
+            if not node.left and not node.right:
+                self.assertIsNotNone(node.symbol, msg='%s, leaf node points to no symbol.' % desc)
+                symbol_set.remove(node.symbol)
+                return
+
+            if node.left and not node.right or node.right and not node.left:
+                self.fail(msg='%s, node has exactly one child.' % desc)
+
+        pre_order_traverse_tree(root, visit)
+        self.assertTrue(not symbol_set, '%s, symbols %s are not found.')
+
+    def test_binary_huffman_code(self):
+        case_class = namedtuple('Case', 'desc symbol_freqs code_len')
+        cases = (
+            case_class(desc='Empty symbol set', symbol_freqs=(), code_len=0),
+            case_class(desc='Single symbol set', symbol_freqs=(1,), code_len=0),
+            case_class(desc='Double symbol set', symbol_freqs=(0.1, 0.9), code_len=1),
+            case_class(desc='Example in the text book',
+                       symbol_freqs=(45, 13, 12, 16, 9, 5), code_len=224),
+        )
+
+        for case in cases:
+            logging.debug('case: ' + case.desc)
+            root = build_binary_huffman_tree(case.symbol_freqs)
+            self._check_binary_code_tree(root, case.symbol_freqs, case.desc)
+            code_len = get_binary_huffman_code_len(root)
+            self.assertAlmostEqual(code_len, case.code_len, 7,
+                                   msg='%s, code length %s != %s' % (case.desc, code_len, case.code_len))
