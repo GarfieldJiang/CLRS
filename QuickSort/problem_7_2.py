b3038fb8aa8c71a24931c1b725965a2214b68952
jehovah0121 <jehovah0121@gmail.com>
2018-09-01 16:12:26 +0800

    Quick sort: problem 7-2, add tests

--- QuickSort/problem_7_2.py
+++ QuickSort/problem_7_2.py
@@ -8,6 +8,7 @@ from Common.common import default_key
 from typing import TypeVar, List, Callable
 from random import randint
 from HeapSort.heap_sort import heap_sort
+from collections import namedtuple
 
 T = TypeVar('T')
 K = TypeVar('K')
@@ -15,13 +16,13 @@ K = TypeVar('K')
 _DEPTH_LIMIT = 32
 
 
-def _partition(array: List[T], i: int, j: int, key: Callable[[T], K]) -> (int, int):
+def _partition(array: List[T], i: int, j: int, key: Callable[[T], K], pivot: int=None) -> (int, int):
     assert(i < j)
     small = 0
     equal = 0
     large = 0
 
-    pivot = randint(i, j)
+    pivot = randint(i, j) if pivot is None else pivot
     array[j], array[pivot] = array[pivot], array[j]
 
     while small + equal + large < j - i:
@@ -76,3 +77,19 @@ class TestQuickSort(unittest.TestCase):
             # print("After: %r" % case.array)
             self.assertTrue(check_sorting_result(before_sorting, case.array, case.offset, case.length, case.key),
                             "Before: %r, After: %r" % (before_sorting, case.array))
+
+    def test_partition(self):
+        case_class = namedtuple('case_class', 'name, array, i, j, pivot, left, right')
+        cases = (
+            case_class(name='2 elements #0', array=[0, 1], i=0, j=1, pivot=0, left=0, right=0),
+            case_class(name='2 elements #1', array=[0, 1], i=0, j=1, pivot=1, left=1, right=1),
+            case_class(name='2 elements #2', array=[0, 0], i=0, j=1, pivot=0, left=0, right=1),
+            case_class(name='2 elements #3', array=[1, 1], i=0, j=1, pivot=1, left=0, right=1),
+            case_class(name='5 different elements', array=[1, 2, 3, 4, 5], i=0, j=4, pivot=2, left=2, right=2),
+            case_class(name='5 elements containing duplicates',
+                       array=[5, 3, 3, 1, 2], i=0, j=4, pivot=2, left=2, right=3)
+        )
+
+        for case in cases:
+            self.assertEqual((case.left, case.right), _partition(case.array, case.i, case.j, default_key, case.pivot),
+                             'Wrong result for case \'%s\'' % case.name)
