0958d0aee0b54a3b4ca5cf0f822a5ae188f958c6
jehovah0121 <jehovah0121@gmail.com>
2019-07-21 18:42:41 +0800

    Number theory: ex 31.2-7

--- P7_SelectedTopics/NumberTheory/gcd.py
+++ P7_SelectedTopics/NumberTheory/gcd.py
@@ -1,9 +1,11 @@
 from unittest import TestCase
+from typing import Sequence, List
 
 
 def extended_euclid(a: int, b: int):
-    assert(a >= 0)
-    assert(b >= 0)
+    """Extended Euclid algorithm for 2 non-negative integers."""
+    assert (a >= 0)
+    assert (b >= 0)
     stack = [a]
     while b != 0:
         a, b = b, a % b
@@ -17,6 +19,29 @@ def extended_euclid(a: int, b: int):
         new_x, new_y = y, x - y * (a // b)
         x, y = new_x, new_y
     return d, x, y
+
+
+def _gcd_internal(seq: Sequence[int], coefs: List[int], n: int):
+    if n == 2:
+        d, x, y = extended_euclid(seq[0], seq[1])
+        coefs[0] = x
+        coefs[1] = y
+        return d
+
+    old_d = _gcd_internal(seq, coefs, n - 1)
+    new_d, x, y = extended_euclid(seq[n - 1], old_d)
+    coefs[n - 1] = x
+    for i in range(n - 1):
+        coefs[i] *= y
+    return new_d
+
+
+def gcd(seq: Sequence[int]):
+    """Ex 31.2-7. GCD algorithm for multiple non-negative integers"""
+    n = len(seq)
+    assert n >= 2
+    coefs = [0] * n
+    return _gcd_internal(seq, coefs, n), coefs
 
 
 class TestGCD(TestCase):
@@ -35,3 +60,19 @@ class TestGCD(TestCase):
             d, x, y = extended_euclid(a, b)
             self.assertEqual(expected_d, d)
             self.assertEqual(expected_d, a * x + b * y)
+
+    def test_gcd(self):
+        cases = (
+            ((1, 2, 3, 4, 5), 1),
+            ((5, 3, 2, 4, 1), 1),
+            ((125, 75, 85, 35), 5),
+            ((312, 542, 798, 324), 2),
+            ((624, 3984, 1056, 3000, 240, 224), 8),
+        )
+        for case in cases:
+            seq = case[0]
+            expected_d = case[1]
+            d, coefs = gcd(seq)
+            self.assertEqual(expected_d, d)
+            # print(d, seq, coefs)
+            self.assertEqual(expected_d, sum(i[0] * i[1] for i in zip(seq, coefs)))
