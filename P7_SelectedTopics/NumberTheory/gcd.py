2c5ff51a84864e29a05a6833bfb3f86a13ff28c9
jehovah0121 <jehovah0121@gmail.com>
2019-07-22 21:36:29 +0800

    Number theory: ex 31.2-8

--- P7_SelectedTopics/NumberTheory/gcd.py
+++ P7_SelectedTopics/NumberTheory/gcd.py
@@ -2,8 +2,19 @@ from unittest import TestCase
 from typing import Sequence, List
 
 
+def euclid(a: int, b: int):
+    """Euclid algorithm."""
+    assert (a >= 0)
+    assert (b >= 0)
+    while b != 0:
+        new_a = b
+        b = a % b
+        a = new_a
+    return a
+
+
 def extended_euclid(a: int, b: int):
-    """Extended Euclid algorithm for 2 non-negative integers."""
+    """Extended Euclid algorithm."""
     assert (a >= 0)
     assert (b >= 0)
     stack = [a]
@@ -44,6 +55,25 @@ def gcd(seq: Sequence[int]):
     return _gcd_internal(seq, coefs, n), coefs
 
 
+def lcm2(a: int, b: int):
+    assert (a >= 0)
+    assert (b >= 0)
+    return 0 if a == 0 or b == 0 else a * b / euclid(a, b)
+
+
+def _lcm_internal(seq: Sequence[int], n: int):
+    if n == 2:
+        return lcm2(seq[0], seq[1])
+    return lcm2(seq[n - 1], _lcm_internal(seq, n - 1))
+
+
+def lcm(seq: Sequence[int]):
+    """Ex 31.2-8. LCM algorithm for multiple non-negative integers"""
+    n = len(seq)
+    assert n >= 2
+    return _lcm_internal(seq, n)
+
+
 class TestGCD(TestCase):
     def test_euclid(self):
         cases = (
@@ -58,6 +88,8 @@ class TestGCD(TestCase):
             b = case[1]
             expected_d = case[2]
             d, x, y = extended_euclid(a, b)
+            d1 = euclid(a, b)
+            self.assertEqual(expected_d, d1)
             self.assertEqual(expected_d, d)
             self.assertEqual(expected_d, a * x + b * y)
 
@@ -78,3 +110,18 @@ class TestGCD(TestCase):
             self.assertEqual(expected_d, d)
             # print(d, seq, coefs)
             self.assertEqual(expected_d, sum(i[0] * i[1] for i in zip(seq, coefs)))
+
+    def test_lcm(self):
+        cases = (
+            ((0, 1), 0),
+            ((1, 0), 0),
+            ((34, 17), 34),
+            ((1, 2, 3, 4, 5), 60),
+            ((5, 3, 1, 4, 2), 60),
+            ((6, 10, 15), 30),
+        )
+        for case in cases:
+            seq = case[0]
+            expected_m = case[1]
+            m = lcm(seq)
+            self.assertEqual(expected_m, m)
